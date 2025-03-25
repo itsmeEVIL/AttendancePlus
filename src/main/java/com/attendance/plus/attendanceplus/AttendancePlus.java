ec475b3f31dfb85ab21468d81ee3d4b71cba1bc3
itsmeEVIL <evilsgonnacry@gmail.com>
2025-03-26 07:40:57 +0800

    uses the new font

--- src/main/java/com/attendance/plus/attendanceplus/AttendancePlus.java
+++ src/main/java/com/attendance/plus/attendanceplus/AttendancePlus.java
@@ -3,6 +3,7 @@ package com.attendance.plus.attendanceplus;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,7 +11,13 @@ import java.io.IOException;
 public class AttendancePlus extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AttendancePlus.class.getResource("Dashboard.fxml"));
+        // Load custom fonts from the resources/fonts directory
+        Font.loadFont(getClass().getResourceAsStream("fonts/Inter-Bold.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Inter-ExtraBold.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("fonts/Inter-SemiBold.ttf"), 14);
+
+        // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(AttendancePlus.class.getResource("fxml/Dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 980, 620);
         stage.setTitle("Attendance+");
         stage.setScene(scene);
